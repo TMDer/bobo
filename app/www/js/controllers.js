7c5b61a4fe627a585871f3db6c5def075cb6d05c
esbb48 <yanru_li@hiiir.com>
2014-12-31 10:52:25 +0800

    update transtions

--- app/www/js/controllers.js
+++ app/www/js/controllers.js
@@ -20,10 +20,15 @@ angular.module('bobo.controllers', [])
     {name: 'Ariel'},
     {name: 'Kir'},
     {name: 'Mak'},
+    {name: 'Caesar'},
     {name: 'Spooky'},
     {name: 'Webber'},
     {name: 'Ivan'},
-    {name: 'Ray'}
+    {name: 'Ray'},
+    {name: 'Matt'},
+    {name: 'Jason'},
+    {name: 'Vincent'},
+    {name: 'Li'}
   ];
 
   $scope.addMsg = function () {
@@ -34,21 +39,24 @@ angular.module('bobo.controllers', [])
     allCircles = document.querySelectorAll('.random-circle')
     angular.forEach(allCircles, function(circle) {
       tempDom = angular.element(circle);
-      circleSize = Math.random() * 100 + 50;
+      circleSize = Math.random() * 120 + 50;
+      tempDom.css('color', '#000')
       tempDom.css('width', circleSize+'px')
       tempDom.css('height', circleSize+'px')
       tempDom.css('line-height', circleSize+'px')
       tempDom.css('background', $scope.getRandomColor())
-      tempDom.css('margin-left', Math.random() * 300 )
+      tempDom.css('margin-top', Math.random() * 100 )
+      tempDom.css('margin-left', Math.random() * 50 )
     });
   }
 
   $scope.getRandomColor = function() {
     var letters = '0123456789ABCDEF'.split('');
     var color = '#';
-    for (var i = 0; i < 6; i++ ) {
+    for (var i = 0; i < 4; i++ ) {
         color += letters[Math.floor(Math.random() * 16)];
     }
+    color += "ff"
     return color;
   }
 
